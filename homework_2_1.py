c15bc5310a957e0e58cc3d8c3554af1bc4ce2c3b
AlexanderKuzmin <kyzmin.alexandr@phystech.edu>
2015-04-12 05:23:31 +0300

    homework_2_1 - generator v 1.1

--- homework_2_1.py
+++ homework_2_1.py
@@ -6,7 +6,7 @@
 '''
 
 import argparse
-import enum
+#import enum  # there are no the module in the contest
 import random
 import sys
 from collections import Counter, defaultdict
@@ -37,22 +37,33 @@ def BinSearch(arr, predicate):
             left = mid + 1
     return -1
 
-def ToProcessText(text, args):
+def ToProcessText(input_text, args):
     '''
-    Processor of the text.
+    Processor of the input_text.
 
-    :param text: str - the text to be needed to process.
+    :param input_text: list of texts - the texts to be needed to process.
     :param args: argparse.ArgumentParser() - the command to be processed and the arguments for it.
 
     :return: result of command process.
     '''
 
     if (args.command == 'tokenize'):
-        return Tokenize(text)
+        if (len(input_text) > 0):
+            return Tokenize(input_text[0])
     elif (args.command == 'probabilities'):
-        return GetProbabilities(text, args.depth)
+        return GetProbabilities(input_text, args.depth)
     elif (args.command == 'generate'):
-        return Generate(text, args.depth, args.size)
+        return Generate(input_text, args.depth, args.size)
+    elif (args.command == 'test'):
+        return UnitTests(input_text, args)
+
+
+# there are no module enum.Enum() in the contest
+class State:
+    alpha = 1
+    digit = 2
+    punctuation = 3
+    space = 4
 
 def Tokenize(text):
     '''
@@ -64,53 +75,55 @@ def Tokenize(text):
     '''
 
     tokens = []
-    state = enum.Enum('state', 'alpha digit space punctuation')
+    # there are no module enum in the contest
+    # state = enum.Enum('state', 'alpha digit space punctuation')
     current_token = []
     current_state = -1
     for letter in text:
         if letter.isalpha():
-            if current_state == state.alpha:
+            if current_state == State.alpha:
                 current_token.append(letter)
             else:
                 tokens.append("".join(current_token))
-                current_state = state.alpha
+                current_state = State.alpha
                 current_token = [letter]
         elif letter.isdigit():
-            if current_state == state.digit:
+            if current_state == State.digit:
                 current_token.append(letter)
             else:
                 tokens.append("".join(current_token))
-                current_state = state.digit
+                current_state = State.digit
                 current_token = [letter]
         elif letter.isspace():
-            if current_state == state.space:
+            if current_state == State.space:
                 current_token.append(letter)
             else:
                 tokens.append("".join(current_token))
-                current_state = state.space
+                current_state = State.space
                 current_token = [letter]
         else:
-            if current_state == state.punctuation:
+            if current_state == State.punctuation:
                 current_token.append(letter)
             else:
                 tokens.append("".join(current_token))
-                current_state = state.punctuation
+                current_state = State.punctuation
                 current_token = [letter]
     tokens.append("".join(current_token))
     return tokens[1:]
 
-def GetNGrams(tokens, n, predicate):
+def GetNGrams(tokens, n, predicate, tokens_counter=defaultdict(Counter)):
     '''
-    get words n_grams from the list of different tokens
+    put words n_grams from the list of different tokens into tokens_counter
 
     :param tokens: list fo str - the list of tokens
     :param n: int - the amount of words in one n-gram
     :param predicate: object - determines the correct predicates: predicate(token) should be true
+    :param tokens_counter: collections.defaultdict - buffer for n-grams (may be not empty!)
 
-    :return: collections.defaultdict of collections.Counter - dict of n-grams
+    :return: defaultdict = collections.defaultdict of collections.Counter - dict of n-grams:
+        'd-gram[:-1]': ('d-gram[-1]' : count)
     '''
 
-    n_grams = defaultdict(Counter)
     n_gram = []
     index = 0
     while (len(n_gram) != n + 1 and index < len(tokens)):
@@ -118,34 +131,39 @@ def GetNGrams(tokens, n, predicate):
             n_gram.append(tokens[index])
         index += 1
 
-    n_grams[tuple(n_gram[:-1])][n_gram[-1]] += 1
+    tokens_counter[tuple(n_gram[:-1])][n_gram[-1]] += 1
 
     while (index < len(tokens)):
         if predicate(tokens[index]):
             n_gram.pop(0)
             n_gram.append(tokens[index])
-            n_grams[tuple(n_gram[:-1])][n_gram[-1]] += 1
+            tokens_counter[tuple(n_gram[:-1])][n_gram[-1]] += 1
         index += 1
-    return n_grams
+    return tokens_counter
 
-def GetProbabilities(text, depth):
+def GetProbabilities(input_text, depth):
     '''
     Get Probabilities of the text chains.
 
-    :param text: str - the text to be needed to tokenize.
+    :param input_text: list of str - the texts to be needed to tokenize.
     :param depth: int - the depth of the chain.
 
     :return: list of defauldict of Counters:
-    'd-gram[:-1]': ('d-gram[-1]' : probability) for each d in 0..depth
+        'd-gram[:-1]': ('d-gram[-1]' : probability) for each d in 0..depth
     '''
 
     list_of_probabilities_counters = []
-    tokens = Tokenize(text)
+    tokens_lists = [Tokenize(text) for text in input_text]
     for current_depth in range(depth + 1):
-        tokens_counter = GetNGrams(tokens, current_depth, lambda x : x.isalpha())
-        total_count = {k : sum(v.values()) for k, v in tokens_counter.items()}
+        tokens_counter = defaultdict(Counter)
+        for tokens in tokens_lists:
+            tokens_counter = GetNGrams(tokens,
+                                       current_depth,
+                                       lambda x: x.isalpha(),
+                                       tokens_counter)
+        total_count = {k: sum(v.values()) for k, v in tokens_counter.items()}
         list_of_probabilities_counters.append(
-            {prefix : {k : v / total_count[prefix] for k, v in counter.items()}
+            {prefix: {k: v / total_count[prefix] for k, v in counter.items()}
                 for prefix, counter in tokens_counter.items()})
     return list_of_probabilities_counters
 
@@ -163,11 +181,11 @@ def Generate(text, depth, size, seed=None):
 
     n_gram_probabilities = GetNGrams(Tokenize(text),
                                      depth,
-                                     lambda x : not x.isspace() and not x.isdigit())
-    total_arrays = {key : list(chain(*[[k] * v for k, v in counter.items()]))
+                                     lambda x: not x.isspace() and not x.isdigit())
+    total_arrays = {key: list(chain(*[[k] * v for k, v in counter.items()]))
                     for key, counter in n_gram_probabilities.items()}
     prefices = list([word for word in n_gram_probabilities.keys()
-                        if len(word) == 0 or word[0][0].isupper()])
+                     if len(word) == 0 or word[0][0].isupper()])
     generated_text = []
     prefix_tokens = []
     if (prefices != []):
@@ -177,7 +195,7 @@ def Generate(text, depth, size, seed=None):
     current_size = sum([len(word) for word in generated_text])
     while(current_size < size):
         current_array = total_arrays.get(tuple(prefix_tokens))
-        if (current_array != None):
+        if (current_array is not None):
             word = random.choice(current_array)
             generated_text.append(word)
             current_size += len(word) + 1
@@ -188,6 +206,15 @@ def Generate(text, depth, size, seed=None):
             generated_text.extend(prefix_tokens)
             current_size += sum([len(word) for word in prefix_tokens]) + 1
     return " ".join(generated_text)
+
+def ReadInputText(args):
+    input_text = []
+    if args.command == 'tokenize':
+        input_text.append(input())
+    else:
+        for text in sys.stdin:
+            input_text.append(text)
+    return input_text
 
 def Print(text, args):
     '''
@@ -211,7 +238,9 @@ def Print(text, args):
                 print(" ".join(prefix))
                 sorted_dict = sorted(depth_dict[prefix].items(), key=itemgetter(0))
                 for word, count in sorted_dict:
-                    print("  {0}: {1:.2}".format(word, count))
+                    print("  {0}: {1:.2f}".format(word, count))
+    elif (args.command == 'test'):
+        None
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -223,6 +252,18 @@ if __name__ == '__main__':
                         help="The maximum depth of chains.")
     parser.add_argument("-s", "--size", action="store", type=int, default=32,
                         help="Approximate amount of words for generating.")
-    args = parser.parse_args()
-    for text in sys.stdin:
-        Print(ToProcessText(text, args), args)
+
+    args_raw_str = input()
+    args = parser.parse_args(args_raw_str.split())
+    input_text = ReadInputText(args)
+    Print(ToProcessText(input_text, args), args)
+
+def UnitTests(input_text, args):
+    '''
+    Unit tests for the program
+
+    :param input_text:
+    :param args:
+
+    :return:
+    '''
